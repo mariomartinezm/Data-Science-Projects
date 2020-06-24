35a0fb521d5065615fe24d00d1916026ddce7885
kingarpharazon <kingarpharazon@live.com.mx>
2020-06-23 21:58:08 -0500

    Update project 'Shelter Animal Outcomes'
    - Clean 'AgeuponOutcome' column.
    - Impute missing values.
    - One hot encode categorical features.

--- Shelter_animal_outcomes/main.py
+++ Shelter_animal_outcomes/main.py
@@ -22,17 +22,18 @@ import pandas as pd
 
 # + _cell_guid="79c7e3d0-c299-4dcb-8224-4455121ee9b0" _uuid="d629ff2d2480ee46fbb7e2d37f6b5fab8052498a"
 train = pd.read_csv('../../Datasets/shelter-animal-outcomes/train.csv.gz')
-train
+train.head(10)
 # -
 
 test = pd.read_csv('../../Datasets/shelter-animal-outcomes/test.csv.gz', index_col=0)
-test
+test.head(10)
 
 # # Exploratory data analysis
 
 # Get columns with missing values:
 
-[col for col in train.columns if train[col].isna().any()]
+print([col for col in train.columns if train[col].isna().any()])
+print([col for col in test.columns if test[col].isna().any()])
 
 # Get the total number of outcome categories:
 
@@ -50,6 +51,94 @@ train['AnimalType'].value_counts()
 
 train['Breed'].value_counts()
 
-# # Target variable and features selection
 
+# # Feature Engineering
 
+# ## Data cleaning
+
+# ### Measure age in days
+
+# The column *AgeuponOutcome* contains the age of an animal at the time the corresponding outcome occurred. However the values for this column contains strings such as "2 years", "2 months", "3 weeks", etc. In order to use these values for training we need to convert them to a common numeric scale. To fix this, we can convert all values to days as follows:
+
+# +
+def extract_age(df, date_part):
+    part = df[df['AgeuponOutcome'].str.contains(date_part) & (df['AgeuponOutcome'].isna() is not True)]['AgeuponOutcome']
+    
+    return part.apply(lambda x: x.split()[0])
+
+age_years = extract_age(train, 'year').astype('int') * 365
+age_months = extract_age(train, 'month').astype('int') * 30
+age_weeks = extract_age(train, 'week').astype('int') * 7
+age_days = extract_age(train, 'day').astype('int')
+
+train.loc[age_years.index, 'AgeuponOutcome'] = age_years
+train.loc[age_months.index, 'AgeuponOutcome'] = age_months
+train.loc[age_weeks.index, 'AgeuponOutcome'] = age_weeks
+train.loc[age_days.index, 'AgeuponOutcome'] = age_days
+
+train['AgeuponOutcome'] = train['AgeuponOutcome'].astype('Int64')
+train['AgeuponOutcome']
+# -
+
+# Now we do the same with the test dataset:
+
+# +
+age_years = extract_age(test, 'year').astype('int') * 365
+age_months = extract_age(test, 'month').astype('int') * 30
+age_weeks = extract_age(test, 'week').astype('int') * 7
+age_days = extract_age(test, 'day').astype('int')
+
+test.loc[age_years.index, 'AgeuponOutcome'] = age_years
+test.loc[age_months.index, 'AgeuponOutcome'] = age_months
+test.loc[age_weeks.index, 'AgeuponOutcome'] = age_weeks
+test.loc[age_days.index, 'AgeuponOutcome'] = age_days
+
+test['AgeuponOutcome']
+# -
+
+# ## Missing Values
+
+# +
+from sklearn.impute import SimpleImputer
+
+cols_missing = ['SexuponOutcome']
+
+si = SimpleImputer(strategy='most_frequent')
+
+imp_cols_train = pd.DataFrame(si.fit_transform(train[cols_missing]))
+imp_cols_train.columns = cols_missing
+
+train[cols_missing] = imp_cols_train
+
+si = SimpleImputer(strategy='mean')
+
+imp_cols_train = pd.DataFrame(si.fit_transform(train[['AgeuponOutcome']]))
+imp_cols_test = pd.DataFrame(si.transform(test[['AgeuponOutcome']]))
+
+train['AgeuponOutcome'] = imp_cols_train
+test['AgeuponOutcome'] = imp_cols_test
+# -
+
+# ## One Hot Encoding
+
+# +
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+categorical = ['SexuponOutcome', 'AnimalType']
+
+ohe = OneHotEncoder(handle_unknown='ignore', sparse=False)
+ohe_cols_train = pd.DataFrame(ohe.fit_transform(train[categorical]))
+ohe_cols_test = pd.DataFrame(ohe.transform(test[categorical]))
+
+ohe_cols_train.index = train.index
+ohe_cols_test.index = test.index
+
+for col in ohe_cols_train.columns:
+    train[col] = ohe_cols_train[col]
+    test[col] = ohe_cols_test[col]
+# -
+
+train.head(10)
+
+test.head(10)
